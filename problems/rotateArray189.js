7014d7cdf4b831f24c903a5ebc9b02e7c644e7a9
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-04-29 01:40:29 +0530

    Updated rotateArray189.js

--- problems/rotateArray189.js
+++ problems/rotateArray189.js
@@ -4,7 +4,7 @@
 //     return result;
 // };
 
-
+// console.log(rotate([1,2,3,4,5,6,7],3))
 
 function reverse(arr,i,j){
     while(i<j){
@@ -14,9 +14,8 @@ function reverse(arr,i,j){
     }
 }
 var rotate = function(nums, k) {
+    k = k%nums.length
     reverse(nums,nums.length-k,nums.length-1)
     reverse(nums,0,nums.length-k-1)
     reverse(nums,0,nums.length-1)
 };
-
-console.log(rotate([1,2,3,4,5,6,7],3))
