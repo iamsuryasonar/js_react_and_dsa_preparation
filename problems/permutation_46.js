2bfd1b321ad4f36540ba62eea827dbfb9773d1d4
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-04-30 23:42:27 +0530

    Update permutation_46.js

--- problems/permutation_46.js
+++ problems/permutation_46.js
@@ -2,23 +2,22 @@
     Intuition is to goto each index and put every element on the right of the array in that place. That will give as permutation of an array.
     Another solution: We can also start with an empty array and keep adding each element of the array in each position until we reach the end of the array.
 */
-function helper(index, nums, result) {
-    let numsCopy = [...nums]
 
+function helper(index, nums, result) {
     if (index === nums.length) {
-        return result.push([...nums])
+        result.push([...nums]);
+        return;
     }
 
     for (let i = index; i < nums.length; i++) {
-        [numsCopy[index], numsCopy[i]] = [numsCopy[i], numsCopy[index]];
-        helper(index + 1, numsCopy, result);
+        [nums[index], nums[i]] = [nums[i], nums[index]];
+        helper(index + 1, nums, result);
+        [nums[index], nums[i]] = [nums[i], nums[index]];
     }
 }
 
-let permutation = (nums) => {
+var permute = function(nums) {
     let result = [];
     helper(0, nums, result);
     return result;
-}
-
-console.log(permutation([1, 2, 3]))
+};
