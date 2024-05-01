85478282ad3025cf6a7bbc74b32022d9444720d7
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-05-02 01:32:39 +0530

    Update happyNumber_202.js

--- problems/happyNumber_202.js
+++ problems/happyNumber_202.js
@@ -1,27 +1,64 @@
-// brute force approach
-var isHappy = function(n) {
-    let set = new Set();
-    let current = n;
+// brute force solution
+
+// var isHappy = function(n) {
+//     let set = new Set();
+//     let current = n;
+
+//     while(true){
+
+//         let arr = current.toString().split('')
+
+//         let sum = 0;
+
+//         for(let i = 0;i<arr.length;i++){
+//             sum += Number(arr[i])**2;
+//         }
+
+//         if(sum === 1){
+//             return true;
+//         }
+
+//         if(set.has(sum)){
+//             return false;
+//         }else{
+//             set.add(sum);
+//             current = sum;
+//         }
+//     }
+// };
+
+
+// optimised solution using fast and slow pointer
+
+function calculateSum(num){
+    let arr = num.toString().split('')
+
+    let sum = 0;
+
+    for(let i = 0;i<arr.length;i++){
+        sum += Number(arr[i])**2;
+    }
     
+    return sum;
+}
+
+var isHappy = function(n) {
+    let slow = n;
+    let fast = n;
+    let currentNum = n;
+
     while(true){
+        slow = calculateSum(slow);
+        fast = calculateSum(calculateSum(fast));
 
-        let arr = current.toString().split('')
-
-        let sum = 0;
-
-        for(let i = 0;i<arr.length;i++){
-            sum += Number(arr[i])**2;
-        }
-
-        if(sum === 1){
+        if(fast === slow && slow === 1){
             return true;
         }
 
-        if(set.has(sum)){
+        if(fast === slow && slow !== 1){
             return false;
-        }else{
-            set.add(sum);
-            current = sum;
         }
+
+        currentNum = slow;
     }
 };
