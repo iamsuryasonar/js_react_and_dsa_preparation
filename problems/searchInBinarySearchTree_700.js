e8c5b882a06a275e967d6194c1ab890e14c92698
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-05-07 13:11:22 +0530

    Create searchInBinarySearchTree_700.js

---
+++ problems/searchInBinarySearchTree_700.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+var searchBST = function(root, val) {
+    let res = null;
+    function helper(root,val){
+        if(!root) return null;
+
+        if(root.val === val){
+            res = root;
+            return;
+        }
+
+        helper(root.left,val)
+        helper(root.right,val)
+    }
+    helper(root,val);
+    return res;
+};
