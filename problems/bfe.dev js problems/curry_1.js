cacb4a461482f3a6beca6bac96cfdb544ca71365
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-05-23 21:16:16 +0530

    Update and rename curry.js to curry_1.js

---
+++ problems/bfe.dev js problems/curry_1.js
@@ -0,0 +1,44 @@
+// Implement a curry() function, which accepts a function and return a curried one.
+
+// solution 1
+
+// function curry(fn) {
+//   let arr = [];
+//   function helper (...args){
+//     if([...arr,...args].length<3){
+//       arr = [...arr,...args]
+//       return helper;
+//     }else{
+//       return fn(...[...arr,...args].splice(0,3));
+//     }
+//   }
+//   return helper;
+// }
+
+
+// solution 2
+
+function curry(fn){
+  return function helper(...args){
+    if(args.length >= fn.length){ // fn.length gives expected parameter length of fn function
+      return fn(...args);
+    }else{
+      return (...newArgs)=> helper(...args,...newArgs);
+    }
+  }
+}
+
+
+const join = (a, b, c) => {
+   return `${a}_${b}_${c}`
+}
+
+const curried = curry(join)(1, 2)
+console.log(curried(3)) //'1_2_3'
+console.log(curried(4)) //'1_2_4'
+
+
+const curriedJoin = curry(join)
+
+console.log(curriedJoin(1, 2, 3)); // '1_2_3'
+console.log(curriedJoin(1,2)(3)); // '1_2_3'
