ced15c97711c1df181d0252b70d8dd79daab09d0
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-07-20 02:02:09 +0530

    Create jumpGame_55.js

---
+++ problems/jumpGame_55.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+
+    // Brute force approach (O(n^2))
+    // Intuition is to know if it is possible to go past 0 (if 0 exist in the path)
+    
+    if(nums.length === 1) return true;
+
+    let maxPossibleJumpIndex = 0;
+    for(let i = 0;i<nums.length;i++){
+        if(maxPossibleJumpIndex === i && nums[i] === 0 && i<nums.length-1) return false;
+
+        let j = 1;
+        while(j<=nums[i] && (j+i)<nums.length){
+            maxPossibleJumpIndex = Math.max(maxPossibleJumpIndex,i+nums[i])
+            j++;
+        }
+    }
+    return true;
+};
