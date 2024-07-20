e30dd45a7f0af93e7586546d9a62123f3d44f6e4
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-07-20 23:20:26 +0530

    Update jumpGame_55.js

--- problems/jumpGame_55.js
+++ problems/jumpGame_55.js
@@ -6,19 +6,35 @@ var canJump = function(nums) {
 
     // Brute force approach (O(n^2))
     // Intuition is to know if it is possible to go past 0 (if 0 exist in the path)
+
+    // if(nums.length === 1) return true;
+
+    // let maxPossibleJumpIndex = 0;
+    // for(let i = 0;i<nums.length;i++){
+    //     // if(maxPossibleJumpIndex === i && nums[i] === 0 && i<nums.length-1) return false;
+    //     if(i > maxPossibleJumpIndex) return false;
+
+    //     let j = 1;
+    //     while(j<=nums[i] && (j+i)<nums.length){
+    //         maxPossibleJumpIndex = Math.max(maxPossibleJumpIndex,i+nums[i])
+    //         j++;
+    //     }
+    // }
+    // return true;
+
     
-    if(nums.length === 1) return true;
+    // Optimised solution (O(n)) Intuition- if a goal can be reached from current index then current index is the new goal.
+    let goal = nums.length-1;
 
-    let maxPossibleJumpIndex = 0;
-    for(let i = 0;i<nums.length;i++){
-        // if(maxPossibleJumpIndex === i && nums[i] === 0 && i<nums.length-1) return false;
-        if(i > maxPossibleJumpIndex) return false;
-
-        let j = 1;
-        while(j<=nums[i] && (j+i)<nums.length){
-            maxPossibleJumpIndex = Math.max(maxPossibleJumpIndex,i+nums[i])
-            j++;
+    for(let i=nums.length-2; i>=0; i--){
+        if(i+nums[i]>=goal){
+            goal = i;
         }
     }
-    return true;
+
+    if(goal===0){
+        return true;
+    }else{
+        return false;
+    }
 };
