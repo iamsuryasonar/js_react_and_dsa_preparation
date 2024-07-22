ecbb22ffae4df6bdf02d3ad4a69c958a697c7816
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-07-23 02:54:59 +0530

    Update jumpGame_55.js

--- problems/jumpGame_55.js
+++ problems/jumpGame_55.js
@@ -4,15 +4,38 @@
  */
 var canJump = function(nums) {
 
-    // Brute force approach (O(n^2))
-    // Intuition is to know if it is possible to go past 0 (if 0 exist in the path)
+    // Solution using dp -----------------
+    let memo = new Array(nums.length).fill(-1);
 
+    function helper(index){
+        if(index>=nums.length-1) return true;
+        if(nums[index]===0) return false;
+
+        if(memo[index]!==-1) return memo[index];
+
+        let maxJump = Math.min(index+nums[index],nums.length-1)
+
+        for(let i = index+1; i<=maxJump; i++){
+            if(helper(i)){
+                memo[index] = true;
+                return true;
+            }
+        }
+
+        memo[index] = false;
+        return false;
+    }
+
+    return helper(0);
+
+    // Brute force approach (O(n^2)) -----------------
+    // Intuition is to know if it is possible to go past 0 (if 0 exist in the path)
+    
     // if(nums.length === 1) return true;
 
     // let maxPossibleJumpIndex = 0;
     // for(let i = 0;i<nums.length;i++){
-    //     // if(maxPossibleJumpIndex === i && nums[i] === 0 && i<nums.length-1) return false;
-    //     if(i > maxPossibleJumpIndex) return false;
+    //     if(maxPossibleJumpIndex < i ) return false;
 
     //     let j = 1;
     //     while(j<=nums[i] && (j+i)<nums.length){
@@ -22,19 +45,19 @@ var canJump = function(nums) {
     // }
     // return true;
 
-    
-    // Optimised solution (O(n)) Intuition- if a goal can be reached from current index then current index is the new goal.
-    let goal = nums.length-1;
 
-    for(let i=nums.length-2; i>=0; i--){
-        if(i+nums[i]>=goal){
-            goal = i;
-        }
-    }
+    // Optimised solution (O(n)) -----------------
+    // let goal = nums.length-1;
 
-    if(goal===0){
-        return true;
-    }else{
-        return false;
-    }
+    // for(let i=nums.length-2; i>=0; i--){
+    //     if(i+nums[i]>=goal){
+    //         goal = i;
+    //     }
+    // }
+
+    // if(goal===0){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
 };
