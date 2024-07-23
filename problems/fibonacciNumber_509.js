12d916ff3f3589206b2ad120f4b6c78bae76fc83
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-07-23 16:41:37 +0530

    Create fibonacciNumber_509.js

---
+++ problems/fibonacciNumber_509.js
@@ -0,0 +1,50 @@
+
+
+// recursion
+// var fib = function(n) {
+//     if(n===0) return 0;
+//     if(n===1) return 1;
+
+//     return fib(n-1)+fib(n-2);
+// };
+
+
+// recursion + memoisation = dp
+// var fib = function(n,dp={}) {
+//     if(n===0) return 0;
+//     if(n===1) return 1;
+
+//     if(dp[n]) return dp[n];
+
+//     return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+// };
+
+
+// tabulation method(dp)
+// var fib = function(n,dp={}) {
+//     dp[0] = 0;
+//     dp[1] = 1;
+
+//     for(let i = 2;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+
+//     return dp[n];
+// };
+
+
+// using two variables
+var fib = function(n) {
+    let prev1 = 0;
+    let prev2 = 1;
+
+    if(n<=0) return 0;
+
+    for(let i = 2;i<=n;i++){
+        let curi = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curi;
+    }
+
+    return prev2;
+};
