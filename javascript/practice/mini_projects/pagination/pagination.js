18460f89e15a9a5935b1ad8e357944aa5fbde123
Surya Sonar <iamsuryasonar@gmail.com>
2025-01-08 00:03:06 +0530

    mini project added

--- javascript/practice/mini_projects/pagination/pagination.js
+++ javascript/practice/mini_projects/pagination/pagination.js
@@ -1,46 +1,76 @@
-async function fetchTodo() {
-    let res = await fetch('https://jsonplaceholder.typicode.com/todos');
-    let todos = res.json();
-    return todos;
-}
+window.onload = async () => {
+    const container = document.getElementById('quote_list');
 
-function populateListItems(todos) {
-    let todo_list = document.getElementById('todo_list');
+    let LIMIT = 10;
+    let skip = 0;
+    let total;
+    let observer = null;
 
-    for (let index in todos) {
-        let list_item = document.createElement('li');
-
-        if (Number(index) === (todos.length - 5)) {
-            list_item.setAttribute('id', 'pagination_flag')
-        }
-        list_item.innerHTML = todos[index].title;
-        todo_list.appendChild(list_item);
+    async function fetchQuotes(limit, skip) {
+        let res = await fetch(`https://dummyjson.com/quotes?limit=${limit}&skip=${skip}`);
+        let result = await res.json();
+        total = result.total;
+        return result;
     }
-}
 
-function listenToPaginate() {
-    let paginationFlag = document.getElementById('pagination_flag')
+    function renderQuotes(quotes) {
+        for (let index = 0; index < quotes.length; index++) {
+            const list_item = document.createElement('li');
+            list_item.style.listStyle = 'none';
 
-    document.addEventListener('scroll', function () {
-        let observer = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                console.log('paginate')
-                // paginate here (Infinite pagination)
+            const title = document.createElement('p');
+            title.style.backgroundColor = 'red';
+            title.style.padding = '10px';
+            title.innerText = quotes[index].quote;
+
+            list_item.append(title);
+
+            if (index === quotes.length - 2) {
+                list_item.setAttribute('id', 'flag');
             }
-        }, {
-            rootMargin: '10px',
-        })
-        if (paginationFlag) {
-            observer.observe(paginationFlag);
+
+            container.appendChild(list_item);
         }
-    })
+    }
+
+    function paginate() {
+
+        if (observer) {
+            observer.disconnect();
+            observer = null;
+        }
+
+        let target = document.getElementById('flag');
+
+        let options = {
+            root: null,
+            rootMargin: '100px'
+        }
+
+        observer = new IntersectionObserver(async ([entry]) => {
+            if (entry.isIntersecting) {
+                if ((skip + LIMIT) <= total) {
+                    target.removeAttribute('id');
+                    skip += LIMIT;
+
+                    render(); // recursively called to render new content
+                } else {
+                    observer.disconnect()
+                    observer = null;
+                }
+            }
+        }, options);
+
+        observer.observe(target);
+    }
+
+    function render() {
+        fetchQuotes(LIMIT, skip).then((res) => {
+            console.log(res);
+            renderQuotes(res.quotes);
+            paginate();
+        })
+    }
+
+    render();
 }
-
-fetchTodo().then((todos) => {
-    populateListItems(todos);
-    listenToPaginate();
-})
-
-
-
-
