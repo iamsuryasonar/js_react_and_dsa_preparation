336ea77e554c33b02c92ec6200d6da8cb962b2f2
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-02 15:26:21 +0530

    Create findDuplicates.js

---
+++ dsa/frontend_blind_75/findDuplicates.js
@@ -0,0 +1,22 @@
+/*
+Given an array of integers numbers, determine whether the array contains any duplicate values. A duplicate is defined as any number that appears more than once in the array.
+
+Input
+numbers: number[]: An array of integers
+
+Examples
+Input: numbers = [5,7,1,3]
+Output: false
+Explanation: All elements in the array are unique.
+
+Input: numbers = [10,7,0,0,9]
+Output: true
+Explanation: 0 appears more than once.
+*/
+
+function findDuplicates(numbers) {
+  
+  let set= new Set(numbers);
+
+  return (set.size !== numbers.length);
+}
