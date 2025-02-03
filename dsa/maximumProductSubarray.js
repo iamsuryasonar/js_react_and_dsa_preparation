97b06577ba779015a5d657e7d8da10ccdc7bbe89
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-03 16:22:39 +0530

    Update maximumProductSubarray.js

--- dsa/maximumProductSubarray.js
+++ dsa/maximumProductSubarray.js
@@ -13,19 +13,29 @@ var maxProduct = function (nums) {
 
         return max; */
 
+
+    
     // optimised approach
+    
     if (nums.length == 0) return 0
 
-    let currentMax = nums[0]
-    let currentMin = nums[0]
-    let ans = nums[0]
+    let numbers =nums;
 
-    for (let i = 1; i < nums.length; i++) {
-        let oldCurrentMax = currentMax
-        currentMax = Math.max(nums[i], currentMax * nums[i], currentMin * nums[i])
-        ans = Math.max(currentMax, ans)
-        currentMin = Math.min(nums[i], currentMin * nums[i], oldCurrentMax * nums[i])
+    let maxProdSoFar = nums[0];
+    let minProdSoFar = nums[0];
+    let result = nums[0];
+
+    for(let i = 1;i<numbers.length;i++){
+        let currentNum = numbers[i];
+
+        let tempMin = minProdSoFar
+
+        minProdSoFar = Math.min(currentNum,maxProdSoFar*currentNum,minProdSoFar*currentNum);
+
+        maxProdSoFar = Math.max(currentNum,maxProdSoFar*currentNum,tempMin*currentNum);
+
+        result = Math.max(result,maxProdSoFar);
     }
 
-    return ans
+    return result;
 };
