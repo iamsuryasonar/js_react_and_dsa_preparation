9335bb0c62b526e366e841919543576902e6c9b9
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-04 20:42:23 +0530

    Update mostCommonElements.js

--- dsa/frontend_blind_75/mostCommonElements.js
+++ dsa/frontend_blind_75/mostCommonElements.js
@@ -5,14 +5,42 @@
  * @param {number} k
  * @return {number[]}
  */
+
+// brute force approach - O(nlogn) because of sorting
 export default function mostCommonElements(numbers, k) {
-  let map = new Map();
+  // let map = new Map();
 
-  for(let i in numbers){
-    map.set(numbers[i],(map.get(numbers[i])||0)+1);
-  }
+  // for(let i in numbers){
+  //   map.set(numbers[i],(map.get(numbers[i])||0)+1);
+  // }
 
-  let res = Array.from(map).sort((a,b)=>b[1]-a[1]).slice(0,k).map((arr)=>arr[0]);
+  // let res = Array.from(map).sort((a,b)=>b[1]-a[1]).slice(0,k).map((arr)=>arr[0]);
 
-  return res;
+  // return res;
+
+  // optimised approach - O(n)
+
+    let map = new Map();
+    let result = [];
+
+    for(let i in numbers){
+        map.set(numbers[i],(map.get(numbers[i])||0)+1);
+    }
+
+    let bucketArr = Array(numbers.length+1).fill(null).map(()=>[]);
+
+    for(let [value,freq] of Array.from(map)){
+        bucketArr[freq].push(value);
+    }
+
+    let filteredArr = bucketArr.filter((item)=>item.length!==0);
+
+    for(let i = filteredArr.length-1;i>=0;i--){
+        for(let j = 0;j<filteredArr[i].length;j++){
+            result.push(filteredArr[i][j]);
+            if(result.length === k) return result;
+        }
+    }
+
+    return result;
 }
