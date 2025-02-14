cf4d14eba22ccb9a34d2ae4f093a45c44906480e
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-14 19:38:05 +0530

    Create maximumWaterBetweenWalls.js

---
+++ dsa/frontend_blind_75/maximumWaterBetweenWalls.js
@@ -0,0 +1,27 @@
+// This problem is not Trapping rain water problem
+
+function maximumWaterBetweenWalls(walls) {
+    let n = walls.length;
+    let max = 0;
+    
+    for(let i=0;i<n;i++){
+        let currMax = 0;
+       for(let j=i+1;j<n;j++){
+            let shortest = Math.min(walls[i],walls[j]);
+            currMax = Math.max(currMax,shortest*(j-i));
+        }
+        max = Math.max(currMax,max);
+    }
+    
+    return max;
+}
+
+
+
+console.log(maximumWaterBetweenWalls([1,4,2,3])) // 6
+// Explanation: Consider two walls (i=1 & i=3) with heights 4 and 3. The water is limited by the shorter wall with height 3, so the container holds 2 (distance) * 3 (shorter height) = 6 units.
+// All other combination of walls result in smaller area.
+
+console.log(maximumWaterBetweenWalls([1,1])) // 1
+console.log(maximumWaterBetweenWalls([1,0])) // 0 
+console.log(maximumWaterBetweenWalls([4,2,4,0,10,6])) // 20   - this is the most important test case.
