e7390356d61247dafed82a41f954e105b4d97385
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-14 19:53:57 +0530

    Update maximumWaterBetweenWalls.js

--- dsa/frontend_blind_75/maximumWaterBetweenWalls.js
+++ dsa/frontend_blind_75/maximumWaterBetweenWalls.js
@@ -1,19 +1,41 @@
 // This problem is not Trapping rain water problem
 
 function maximumWaterBetweenWalls(walls) {
+    // brute force approach - O(n^2)
+    // let n = walls.length;
+    // let max = 0;
+    
+    // for(let i=0;i<n;i++){
+    //     let currMax = 0;
+    //    for(let j=i+1;j<n;j++){
+    //         let shortest = Math.min(walls[i],walls[j]);
+    //         currMax = Math.max(currMax,shortest*(j-i));
+    //     }
+    //     max = Math.max(currMax,max);
+    // }
+    
+    // return max;
+
+   // Optimised approach - O(n)
     let n = walls.length;
     let max = 0;
     
-    for(let i=0;i<n;i++){
-        let currMax = 0;
-       for(let j=i+1;j<n;j++){
-            let shortest = Math.min(walls[i],walls[j]);
-            currMax = Math.max(currMax,shortest*(j-i));
+    let i = 0,j=n-1;
+    
+    while(i<j){
+        let shortest = Math.min(walls[i],walls[j]);
+        
+        max = Math.max(max,shortest*(j-i));
+        
+        if(shortest === walls[i]){
+            i++;
+        }else{
+           j--; 
         }
-        max = Math.max(currMax,max);
     }
     
     return max;
+    
 }
 
 
