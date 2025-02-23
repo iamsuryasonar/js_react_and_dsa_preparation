a6a0f14f5a8c7f305796982a865d5a50209d98ac
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-02-23 21:57:33 +0530

    Create transitionPoint.js

---
+++ dsa/gfg/transitionPoint.js
@@ -0,0 +1,9 @@
+//https://www.geeksforgeeks.org/problems/find-transition-point-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution {
+    transitionPoint(arr) {
+        // code here
+        let index = arr.indexOf(1);
+        return index;
+    }
+}
