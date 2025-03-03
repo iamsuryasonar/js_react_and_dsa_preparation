dd10b801ad993e35dcb87bbe0044622946fdd59a
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-03 16:02:35 +0530

    Update reverseLinkedList_206.js

--- dsa/reverseLinkedList_206.js
+++ dsa/reverseLinkedList_206.js
@@ -1,9 +1,13 @@
 var reverseList = function(head) {
     if(!head?.next) return head;
     
-    let newHead = reverseList(head.next);
+    // recurse till the last node (which will be the new head of the list)
+    let newHead = reverseList(head.next); 
 
+    // while returning from the recursion perform reverse operation on two nodes
+    // next of second node will point to first node
     head.next.next = head;
+    // first node will point to null to avoid circular reference
     head.next = null;
 
     return newHead;
