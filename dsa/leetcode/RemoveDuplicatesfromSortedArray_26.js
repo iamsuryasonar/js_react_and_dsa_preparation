4b92e7c74fc4567c6310e7ea0b8a660ecf22c406
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-08 01:48:01 +0530

    Update RemoveDuplicatesfromSortedArray_26.js

--- dsa/leetcode/RemoveDuplicatesfromSortedArray_26.js
+++ dsa/leetcode/RemoveDuplicatesfromSortedArray_26.js
@@ -1,7 +1,3 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var removeDuplicates = function(nums) {
     let i = 0;
     let j = 0;
@@ -13,4 +9,31 @@ var removeDuplicates = function(nums) {
         }
     }
     return i+1
+};
+
+
+var removeDuplicates = function(nums) {
+    // let start = 0;
+    // for(let i = 1;i<nums.length; i++){
+    //     if(nums[start] !== nums[i]){
+    //         start++;
+    //         nums[start] = nums[i];
+    //     }
+    // }
+
+    // return start+1;
+
+    let  i = 0;
+    let j = 0;
+
+    while(j<nums.length){
+        while(nums[i]===nums[j])j++;
+
+        if(nums[i]!==nums[j] && j<nums.length){
+            i++;
+            nums[i] = nums[j];
+        }
+        j++;
+    }
+    return i+1;
 };
