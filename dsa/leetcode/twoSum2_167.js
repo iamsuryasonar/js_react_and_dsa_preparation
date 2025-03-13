a3ac3ace14377e59fd44afeb8de48dac4a5768d6
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-14 00:03:48 +0530

    Create twoSum2_167.js

---
+++ dsa/leetcode/twoSum2_167.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i=0
+    let j = numbers.length-1;
+
+    while(i<j){
+
+        if(numbers[i]+numbers[j]<target){
+            i++;
+        }else if(numbers[i]+numbers[j]> target){
+            j--;
+        }else{
+            return [i+1,j+1];
+        }
+    }
+
+    return [];
+
+};
