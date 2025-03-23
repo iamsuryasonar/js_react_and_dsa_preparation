9aea23eb78cd3dc46ab3d2664f4325a34b63df4f
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-23 18:11:34 +0530

    Update maxConsecutiveOnesIII_1004.js

--- dsa/leetcode/maxConsecutiveOnesIII_1004.js
+++ dsa/leetcode/maxConsecutiveOnesIII_1004.js
@@ -28,3 +28,33 @@ var longestOnes = function (nums, k) {
     return maxLen;
 
 };
+
+
+// var longestOnes = function (nums, k) {
+
+//     let maxLen = 0;
+//     let maxF = 0;
+//     let i = 0;
+//     let j = 0;
+//     let map = new Map();
+
+//     while (j < nums.length) {
+//         map.set(nums[j], (map.get(nums[j])|| 0) + 1);
+//         maxF = map.get(1)||0;
+
+//         let changes = (j - i + 1) - maxF;
+
+//         if (changes <= k) {
+//             maxLen = Math.max(maxLen, j - i + 1);
+//         } else {
+//             map.set(nums[i], map.get(nums[i]) - 1);
+//             if (map.get(nums[i]) === 0) map.delete(nums[i]);
+//             maxF = map.get(1)||0;
+//             i++;
+//         }
+//         j++;
+//     }
+
+//     return maxLen;
+// };
+
