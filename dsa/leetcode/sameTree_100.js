042de4b43a8c28a6c3d6c665bce1cf4112afef7c
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-25 18:42:22 +0530

    Update sameTree_100.js

--- dsa/leetcode/sameTree_100.js
+++ dsa/leetcode/sameTree_100.js
@@ -20,14 +20,27 @@
 // 5. Return the logical AND of the results from steps 3 and 4.
 
 
-var isSameTree = function(p, q) {
+// var isSameTree = function(p, q) {
 
-    if(!p && !q) return true;
+//     if(!p && !q) return true;
    
-    if(p===null ||q===null||p.val !== q.val) {
-        return false;
-    }
+//     if(p===null ||q===null||p.val !== q.val) {
+//         return false;
+//     }
 
-    return(isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
+//     return(isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
  
+// };
+
+//another solution
+var isSameTree = function(p, q) {
+    if(!p && !q) return true;
+    if(p && !q || !p && q) return false;
+
+    if(p.val!==q.val) return false;
+ 
+    let left = isSameTree(p.left,q.left);
+    let right = isSameTree(p.right,q.right);
+
+    return left && right;
 };
