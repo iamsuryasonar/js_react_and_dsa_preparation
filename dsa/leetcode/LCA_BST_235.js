bc2cba2de6a10b242c9f8be6f03fb5bd8c66f7b6
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2025-03-26 20:20:20 +0530

    Update LCA_BST_235.js

--- dsa/leetcode/LCA_BST_235.js
+++ dsa/leetcode/LCA_BST_235.js
@@ -13,15 +13,29 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root) return null;
+    // recursive
+    // if(!root) return null;
 
-    let curr = root.val;
+    // let curr = root.val;
 
-    if(curr < p.val && curr < q.val){
-        return lowestCommonAncestor(root.right, p, q);
-    }else if(curr > p.val && curr > q.val){
-        return lowestCommonAncestor(root.left, p, q);
+    // if(curr < p.val && curr < q.val){
+    //     return lowestCommonAncestor(root.right, p, q);
+    // }else if(curr > p.val && curr > q.val){
+    //     return lowestCommonAncestor(root.left, p, q);
+    // }else{  // if p and q are in different sides
+    //     return root;    
+    // }
+
+    // iterative
+    let curr = root;
+    while(curr) {
+
+        if(p.val > curr.val && q.val > curr.val) {
+            curr = curr.right;
+        } else if(p.val < curr.val && q.val < curr.val) {
+            curr = curr.left;
+        } else { // if p and q are in different sides
+            return curr;
+        }
     }
-
-    return root;
 };
