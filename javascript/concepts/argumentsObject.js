4d3a59857074d85899bab4bb32827f8eb103f854
ganesh shah <shahganesh777@gmail.com>
2025-03-26 09:29:10 +0530

    Corrected typo in argumentObject file and added example for rest param usage for arrow func

--- javascript/concepts/argumentsObject.js
+++ javascript/concepts/argumentsObject.js
@@ -1,4 +1,4 @@
-/* arguments object is a special local variable available within all functions(accept arrow functions, for it we can use rest parameter)
+/* arguments object is a special local variable available within all functions(except arrow functions, for it we can use rest parameter)
 allows us to access all arguments passed to the function, even if not defined as named parameters */
 
 function example() {
@@ -7,3 +7,12 @@ function example() {
 }
 
 example(1, 2, 3, 4)
+
+
+// usage of rest param in arrow functions as an alternative to arguments object
+// Rest parameter is an improved way to handle function parameter, allowing us to more easily handle various input as parameters in a function.
+const arrowFunction = (...args) => {
+    console.log(args); // Logs: [1, 2, 3]
+  };
+  arrowFunction(1, 2, 3);
+  
